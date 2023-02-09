619c68cf19bfbe5d375d69d9a99d659653c22d7c
mkoch2 <121644466+mkoch2@users.noreply.github.com>
2023-02-08 21:05:02 -0500

    Delete T040_data_analyzer directory

--- T040_data_analyzer/check_equal.py
+++
@@ -1,9 +0,0 @@
-# Matthew Koch, Carolyn Grum, Beth Pearson
-def check_equal(expected, actual):
-    if(expected == actual):
-        #print('Passed: ')
-        return True
-
-    else:
-        #print('Failed: ')
-        return False
